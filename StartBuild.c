370c532b124b3354e36d4cf2cc99a1849cbe8a31
yee <nthnlius@gmail.com>
2020-11-18 11:51:59 +0700

    yee

--- StartBuild.c
+++ StartBuild.c
@@ -1,17 +1,17 @@
 #include "queuelinier.h"
 #include "queuelinier.c"
 
-void StartBuild(Queue Q){
-    printf("=================================================================================\n");
-    printf("Item yang dibutuhkan oleh pesanan ini : \n");
-    printf("%d\n", Q.Tab[Q.HEAD]);
-    printf("=================================================================================\n");
-}
-int main(){
-    Queue Q;
-    int A = 7;
-    Q = CreateQueue(7);
-    Push(&Q, A);
-    StartBuild(Q);
-    return 0;
+void StartBuild(Queue Q, IdxType * CurrentlyBuilt){
+    if (*CurrentlyBuilt == NIL){
+        printf("=================================================================================\n");
+        printf("Item yang dibutuhkan oleh pesanan ini : \n");
+        printf("%d\n", Q.Tab[Q.HEAD]);
+        *CurrentlyBuilt = Q.HEAD;
+        printf("=================================================================================\n");
+    }
+    else{
+        printf("=================================================================================\n\n\n");
+        printf("tolong kerjakan pesanan yang sudah terdaftar terlebih dahulu!\n\n\n");
+        printf("=================================================================================\n");
+    }
 }
