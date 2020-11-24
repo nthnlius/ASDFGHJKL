f8c839975522a75c915f8949f45eaa87e1b4d23b
yee <nthnlius@gmail.com>
2020-11-25 00:50:26 +0700

    perbaikan

--- StartBuild.c
+++ StartBuild.c
@@ -1,17 +1,50 @@
 #include "queuelinier.h"
 #include "queuelinier.c"
 
+void printbatas(){
+    printf("\n=================================================================================\n");
+}
 void StartBuild(Queue Q, IdxType * CurrentlyBuilt){
     if (*CurrentlyBuilt == NIL){
-        printf("=================================================================================\n");
-        printf("Item yang dibutuhkan oleh pesanan ini : \n");
-        printf("%d\n", Q.Tab[Q.HEAD]);
-        *CurrentlyBuilt = Q.HEAD;
-        printf("=================================================================================\n");
+        if (Q.HEAD != NIL){
+            printbatas();
+            printf("Item yang dibutuhkan oleh pesanan ini : \n");
+            printf("%d", Q.Tab[Q.HEAD]);
+            *CurrentlyBuilt = Q.HEAD;
+            printbatas();
+        }
+        else {
+            printbatas();
+            printf("Selamat! Semua pesanan telah selesai dikerjakan");
+            printbatas();
+        }
     }
     else{
-        printf("=================================================================================\n\n\n");
-        printf("tolong kerjakan pesanan yang sudah terdaftar terlebih dahulu!\n\n\n");
-        printf("=================================================================================\n");
+        printbatas();
+        printf("tolong kerjakan pesanan yang sudah terdaftar terlebih dahulu!");
+        printbatas();
     }
 }
+// int main(){
+//     Queue Q;
+//     int A, B, C, D, E;
+//     int Qi = NIL;
+//     int Qmax = 10;
+//     A = 7;
+//     B = 2;
+//     C = 9;
+//     D = 15;
+//     E = 11;
+//     Q=CreateQueue(Qmax);
+//     StartBuild(Q, &Qi);
+//     Push(&Q, A);
+//     Push(&Q, B);
+//     Push(&Q, C);
+//     Push(&Q, D);
+//     Push(&Q, E);
+//     StartBuild(Q, &Qi);
+//     A = Pop(&Q);
+//     Qi = NIL;
+//     StartBuild(Q, &Qi);
+//     StartBuild(Q, &Qi);
+// }
