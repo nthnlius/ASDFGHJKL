35e67058aab3b62e615d6ee49993932d717bec12
M. kautsar F <68513233+MKautsarF@users.noreply.github.com>
2020-11-28 19:29:04 +0700

    Update build.c

--- build.c
+++ build.c
@@ -16,21 +16,27 @@ void addcomponent(){
     printf("ENTER COMMAND: ");
     scanf("%s", ac);
     if (strcmp(ac, "ADDCOMPONENT")==0){
-        if( CurrentlyBuilt != Nil ){// udah startbuild jadi bisa addcomponent
+        if( CurrentlyBuilt != Nil ){ // udah startbuild jadi bisa addcomponent
+            // harus nya dari sini loop sampe berhasil dipasang kalau mau nampilin semua inventory
+            // tapi kalau satu2 per kategori harusnya gausa bikin loop
             printf("Komponen yang telah terpasang: ");
             // tampilin komponen yang terpasang
-            printf("Komponen yang tersedia: ");
+            // nampilin stack build (cek teststack.c)
+            // hitung total stack ( S.TOP+1 ) kalo masih 0 ke motherboard dan seterusnya, kalo 1 ke 
+            printf("Komponen motherboard yang tersedia: ");
             // tampilin inventory
             // TulisIsiTab(inventorymotherboard)
+            // tampilin inventory yg >0, kalo barang yang bernilai = 0 gausa tampilin (how ya)
             for (int j = 0;j < 4;j++){
-                printf("%d. %s, \n", j+1, motherboard[j]);
+                printf("%d. %s \n", j+1, motherboard[j]);
             }
             printf("Komponen motherboard yang ingin dipasang: ");
             scanf("%d", &i);
             Push(&S, (*inventorymotherboard)[i-1] );
             jumlahmotherboard[i-1] = jumlahmotherboard[i-1] - 1;
             printf("Komponen motherboard berhasil dipasang!");
-
+            // bikin command BACK buat keluar dari addcomponen ini
+            
         }
 
         else{ 
