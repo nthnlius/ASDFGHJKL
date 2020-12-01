01696b66e18e6b5195ccdce8b0fdb6ecf836dbde
ilhamalvindo <ilhamalvindo@gmail.com>
2020-12-01 20:36:45 +0700

    update arraydin(2)

--- arraydin.c
+++ arraydin.c
@@ -1,9 +1,3 @@
-/* MODUL TABEL INTEGER */
-/* Berisi definisi dan semua primitif pemrosesan tabel integer */
-/* Penempatan elemen selalu rapat kiri */
-/* Versi II : dengan banyaknya elemen didefinisikan secara eksplisit,
-   memori tabel dinamik */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "arraydin.h"
