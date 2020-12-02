0e2b1523e4e9e849d8d7e7d092189ab484d7a740
M. kautsar F <68513233+MKautsarF@users.noreply.github.com>
2020-12-02 21:14:54 +0700

    Update build.c

--- build.c
+++ build.c
@@ -3,11 +3,13 @@
 #include <string.h>
 #include "stack.c"
 // #include "StartBuild.c"
+// #include "inventory.c"
 
+// skr versi inventory arrydin tapi blom dicoba karena bingung :<
 int AddComponent(char ac[20]){
     // di startbuild createstack harusnya
     Stack S;
-    CreateEmptyStack(&S);
+    // CreateEmptyStack(&S);
     int i,j,z,w;
     j = 0;
     z = 0;
@@ -15,78 +17,79 @@ int AddComponent(char ac[20]){
 
     //ini masih pake inventory asal
 
-//     char *X[12] = {"Ac", "B", "C", "D",
-//                   "E", "F", "G", "H",
-//                   "I", "J", "K","L"}; 
+    // char *X[12] = {"Ac", "B", "C", "D",
+    //               "E", "F", "G", "H",
+    //               "I", "J", "K","L"}; 
 
-//     int invetoryX[] = {9, 8, 6, 0,
-//                        9, 8, 6, 7, 
-//                        9, 8, 6, 7};
+    // int invetoryX[] = {9, 8, 6, 0,
+    //                    9, 8, 6, 7, 
+    //                    9, 8, 6, 7};
 
     int CurrentlyBuilt = 1;
-//     printf("ENTER COMMAND: ");
-//     scanf("%s", ac);
+    // printf("ENTER COMMAND: ");
+    // scanf("%s", ac);
 
     if( (strcmp(ac, "ADDCOMPONENT")==0) && ( CurrentlyBuilt != Nil )){
         while(strcmp(ac, "ADDCOMPONENT")==0){
             printf("Komponen yang telah terpasang: \n");
-            // PrintStack(S);
+            PrintStack(S);
             printf("Komponen yang tersedia: \n");
-            // PrintStack(S);
-            while(j<12){
-                printf("%d. %s stock: %d \n", j+1, X[j], invetoryX[j]);
+            while(j<12){ // NGEPRINT LIST KOMPONEN 
+                printf("%d. %s stock: %d\n", j+1, T.A[j], T.TI[j]);
                 j++;
             }
             printf("Komponen yang stocknya 0 tidak dapat dipasang! \n");
             printf("Komponen X yang ingin dipasang: ");
-            scanf("%d", &i);
+            scanf("%d", &i); //nanti input (i) ini dikurang 1 untuk menyesuaikan index
 
-            if( invetoryX[i-1] == 0){
-                printf("Komponen %s tidak tersedia! \n", X[i-1]);
+            if( T.TI[i-1]== 0){ //kalau user milih komponen yang stocknya 0
+                printf("Komponen %s tidak tersedia! \n", T.A[i-1]);
             }
-            else{
-                // Push(&S, (X)[i-1]);
-                printf("%s\n", X[i-1]);
-                printf("%d\n",invetoryX[i-1]);
+            else{ //kalo user milih komponennya bener
+                Push(&S, T.A[i-1]); //nge push komponen pilihan ke stack (stack yg udh di create di startbuild)
+                printf("Komponen %s sedang dipasang\n", T.A[i-1); //memperlihatkan komponen apa yg ke push
+                T.TI[i-1] = T.TI[i-1] -1; //ngurangin 1 stock dr komponen yg dipilih
+                printf("Stock sekarang: %d\n",T.TI[i-1]); //ngeliatin stock 
                 printf("Komponen berhasil dipasang!\n");
             }
-            printf("ENTER COMMAND: ");
+            printf("ENTER COMMAND: "); //kalo mau addcomponent lagi
             scanf("%s", ac);
             j = 0;
         }
     }
-    else{
-        printf("silahkan 'startbuild' terlebih dahulu\n");
+    else{ // ini kalo blom startbuild alias startbuild == Nil
+        printf("silahkan 'startbuild' terlebih dahulu\n"); 
     }
 }
 
 int RemoveComponent(char ac[20]){
     // di startbuild createstack harusnya
+    Stack S;
     int i,j,z,w;
     j = 0;
     z = 0;
     w = 0;
     i = 0;
+    infotype X; //variabel dummy untuk memenuhi parameter Pop
 
     //ini masih pake inventory asal
 
-//     char *X[12] = {"Ac", "B", "C", "D",
-//                   "E", "F", "G", "H",
-//                   "I", "J", "K","L"}; 
+    // char *X[12] = {"Ac", "B", "C", "D",
+    //               "E", "F", "G", "H",
+    //               "I", "J", "K","L"}; 
 
-//     int invetoryX[] = {9, 8, 6, 0,
-//                        9, 8, 6, 7, 
-//                        9, 8, 6, 7};
+    // int invetoryX[] = {9, 8, 6, 0,
+    //                    9, 8, 6, 7, 
+    //                    9, 8, 6, 7};
 
-//     char ac[20];
     int CurrentlyBuilt = 1;
-//     printf("ENTER COMMAND: ");
-//     scanf("%s", ac);
+    // printf("ENTER COMMAND: ");
+    // scanf("%s", ac);
 
     if( (strcmp(ac, "REMOVECOMPONENT")==0) && ( CurrentlyBuilt != Nil )){
         while(strcmp(ac, "REMOVECOMPONENT")==0){
-            // Pop(&S,(&X)[i-1]);
-            printf("Komponen berhasil dicopot! \n");
+            Pop(&S,&X);
+            printf("Komponen %c berhasil dicopot! \n", X); //ini %c ngikutin dari stack.c nya printstack
             printf("ENTER COMMAND: ");
             scanf("%s", ac);
         }
